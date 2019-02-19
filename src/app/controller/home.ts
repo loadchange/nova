9d6f2b7f2cb6fdce89a6f72ce73d4d693f8b60bd
LoadChange <soocto@gmail.com>
2019-02-19 23:55:15 +0800

    Init

---
+++ src/app/controller/home.ts
@@ -0,0 +1,19 @@
+import { Context, inject, controller, get, provide } from 'midway';
+
+@provide()
+@controller('/')
+export class HomeController {
+
+  @inject()
+  ctx: Context;
+
+  @get('/')
+  async index() {
+    this.ctx.body = `Welcome to midwayjs!`;
+  }
+
+  @get('/test')
+  async index2() {
+    this.ctx.body = `Welcome to midwayjs test!`;
+  }
+}
