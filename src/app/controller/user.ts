a087443abc616d43e15ef5411a521c6056667d96
LoadChange <soocto@gmail.com>
2019-10-17 20:37:05 +0800

    user graphql

--- src/app/controller/user.ts
+++ src/app/controller/user.ts
@@ -1,8 +1,10 @@
-import { Context, controller, get, inject, provide } from 'midway';
-import { IUserService, IUserResult } from '../../interface';
+import { Context, inject, provide } from 'midway';
+import { IUserService } from '../../interface';
+import { User } from '../../entity';
+import { Resolver, Query, Arg } from 'type-graphql';
 
+@Resolver(User)
 @provide()
-@controller('/user')
 export class UserController {
 
   @inject()
@@ -11,10 +13,14 @@ export class UserController {
   @inject('userService')
   service: IUserService;
 
-  @get('/:id')
-  async getUser(): Promise<void> {
-    const id: number = this.ctx.params.id;
-    const user: IUserResult = await this.service.getUser({id});
-    this.ctx.body = {success: true, message: 'OK', data: user};
+  @Query(returns => User)
+  async getUser(@Arg('id')id: number) {
+    return this.service.getUser({ id });
+    // return {
+    //   id,
+    //   username: 'mockedName',
+    //   phone: '12345678901',
+    //   email: 'xxx.xxx@xxx.com',
+    // };
   }
 }
