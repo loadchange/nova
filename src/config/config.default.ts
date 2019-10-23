2cf7097178ff924b8b73954ad857e5754da46794
LoadChange <soocto@gmail.com>
2019-10-23 22:23:07 +0800

    default config

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,15 +1,32 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'midway';
 
-export type DefaultConfig = PowerPartial<EggAppConfig>
+export type DefaultConfig = PowerPartial<EggAppConfig>;
 
 export default (appInfo: EggAppInfo) => {
-  const config = <DefaultConfig>{};
+  const config = {} as DefaultConfig;
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1580204721503_7108';
 
   // add your config here
-  config.middleware = [];
+  config.middleware = [ 'graphql' ];
+
+  config.graphql = {
+    path: '/graphql',
+    cors: false,
+    bodyParserConfig: true,
+    disableHealthCheck: true
+  };
+
+  config.security = {
+    csrf: {
+      ignore: '/graphql',
+    }
+  };
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+  };
 
   return config;
 };
